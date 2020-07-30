dbbaf32af2804f37e9dd1111fe657cf09e7486fb
Lucas Nascimento <lucassnascimento20@gmail.com>
2020-07-30 14:14:17 -0300

    first project with nextJS + react

---
+++ pages/index.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "isomorphic-fetch";
+import Link from 'next/link'
+
+const Home = ({ repositories }) => (
+  <div>
+      <Link href="/blog">
+          <a>Blog</a>
+      </Link>
+    {repositories.map((repo) => (
+      <h1 key="repo.id">{repo.name}</h1>
+    ))}
+  </div>
+);
+
+Home.getInitialProps = async () => {
+  const response = await fetch(
+    "https://api.github.com/users/lucassanascimento/repos"
+  );
+  const repositories = await response.json();
+
+  console.log("repositories");
+
+  return { repositories };
+};
+
+export default Home;
